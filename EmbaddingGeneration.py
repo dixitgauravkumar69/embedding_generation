a17cb3bd9f2c074027e544d0df6565f31049761a
dixitgauravkumar69 <153165437+dixitgauravkumar69@users.noreply.github.com>
2026-05-07 11:37:48 +0530

    Fix comments and update API message

--- EmbaddingGeneration.py
+++ EmbaddingGeneration.py
@@ -4,31 +4,27 @@ from sentence_transformers import SentenceTransformer
 
 app = FastAPI()
 
-# Load AI Embedding Model
 model = SentenceTransformer(
-    'sentence-transformers/all-MiniLM-L6-v2'
+    'sentence-transformers/all-MiniLM-L6-v2',
+    device='cpu'
 )
 
 class TextRequest(BaseModel):
     text: str
 
-
 @app.post("/embedding")
 def embedding(req: TextRequest):
 
-    # Clean text
     text = req.text.lower().strip()
 
-    # Generate semantic embedding
     embedding_vector = model.encode(text).tolist()
 
     return {
         "embedding": embedding_vector
     }
 
-
 @app.get("/")
 def home():
     return {
-        "message": "AI Embedding API Running 🚀"
+        "message": "Embedding API Running 🚀"
     }
